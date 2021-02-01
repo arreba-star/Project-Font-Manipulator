545cc8f7d30f768b3e985950d2612e5365730f93
arreba-star <75185381+arreba-star@users.noreply.github.com>
2021-01-31 21:31:49 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function setup(){
+
+    video = createCapture(VIDEO);
+    video.size(550 ,500);
+    canvas = createCanvas(550 ,550);
+    canvas.position(560 , 100);
+
+    poseNet = ml5.poseNet(video , modelLoaded);
+    poseNet.on('pose' , gotPoses);
+}
+
+function draw(){
+    background('#969A97');
+    document.getElementById("width/hieght").innerHTML = "Width And Height of a Square will be =" + difference;   
+
+    fill('#F90093');
+    stroke('#F90093');
+    textSize(difference);
+    text('Areeba', noseX, noseY);
+}
+function modelLoaded(){
+    console.log('PoseNet Is Initialized!');
+
+}
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("Nose X" + noseX + "Nose Y" + noseY);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = Math.floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX + "difference = " + difference);
+
+    }
+}
